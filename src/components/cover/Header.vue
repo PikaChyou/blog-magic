a7b9594cb5e3ee6aabe987947832428e46bd8f83
Uiwzen <qiuweizhong2003@gmail.com>
2024-07-21 22:43:10 +0800

    feat:添加了一个搜素的按钮（逻辑未添加）以及添加了flexsearch依赖项

--- src/components/cover/Header.vue
+++ src/components/cover/Header.vue
@@ -5,13 +5,18 @@
                 <router-link to="/">Home</router-link>
             </slot>
         </nav>
-        <div class="header-right">
-            <svg class="moon-btn" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
-                @click="switchMode">
+        <div class="header-right flex-c">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" @click="switchMode">
                 <path
                     d="M11.286 22C13.337 22 15 20.42 15 18.47c0-1.544-1.045-2.857-2.5-3.336C12.295 13.371 10.72 12 8.81 12c-2.052 0-3.715 1.58-3.715 3.53c0 .43.082.844.23 1.226a2.949 2.949 0 0 0-.54-.05C3.248 16.706 2 17.89 2 19.353C2 20.815 3.247 22 4.786 22z" />
                 <path
                     d="M2.716 15.723c.278-.148.574-.268.882-.354C3.686 12.6 6.056 10.5 8.81 10.5c.172 0 .343.008.511.024A6.495 6.495 0 0 1 9 8.5a6.496 6.496 0 0 1 3.143-5.567C12.54 2.693 12.463 2 12 2C6.477 2 2 6.477 2 12a9.97 9.97 0 0 0 .716 3.723M22 12c0 4.266-2.671 7.908-6.432 9.345a4.865 4.865 0 0 0 .932-2.874a4.907 4.907 0 0 0-1.457-3.487a6.496 6.496 0 0 0 6.024-3.127c.24-.396.933-.32.933.143" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                <g fill="none" stroke="#fdfdfd" stroke-width="1.5">
+                    <circle cx="11.5" cy="11.5" r="9.5" />
+                    <path stroke-linecap="round" d="M18.5 18.5L22 22" />
+                </g>
             </svg>
         </div>
     </div>
@@ -89,7 +94,7 @@ const switchMode = () => {
     float: right;
 }
 
-.moon-btn {
+.header-right>svg {
     margin: 1rem;
     width: 1.75rem;
     height: 1.75rem;
